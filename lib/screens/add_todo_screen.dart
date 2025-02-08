fea8f4124dd728d4caef123cd3d17d9f2daa7e6c
Jayakrishnan M <jk.jayakrishnan27@gmail.com>
2025-02-08 11:48:19 +0530

    add: separate add todos screen

---
+++ lib/screens/add_todo_screen.dart
@@ -0,0 +1,99 @@
+import 'package:contineu/services/database_service.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AddTodoScreen extends StatefulWidget {
+  const AddTodoScreen({super.key, this.todoTitle, this.id});
+
+  final String? todoTitle;
+  final String? id;
+
+  @override
+  State<AddTodoScreen> createState() => _AddTodoScreenState();
+}
+
+class _AddTodoScreenState extends State<AddTodoScreen> {
+  final TextEditingController _controller = TextEditingController();
+  final DatabaseService _databaseService = DatabaseService();
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.todoTitle != null) {
+      _controller.text = widget.todoTitle!;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        border: null,
+        backgroundColor: Colors.transparent,
+        middle: Text(
+          widget.id!=null ? "Edit Task" : "Add Task",
+          style: TextStyle(
+            fontSize: 32,
+          ),
+        ),
+      ),
+      child: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+          child: Column(
+            children: [
+              // const SizedBox(height: 100,),
+              SizedBox(
+                height: 100,
+                child: CupertinoTextField(
+                  controller: _controller,
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                        color: CupertinoColors.darkBackgroundGray,
+                        width: 2,
+                      ),
+                      borderRadius: BorderRadius.circular(10)),
+                ),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              FilledButton.icon(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: CupertinoColors.darkBackgroundGray,
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+                onPressed: () async {
+                  if (widget.id != null) {
+                    if (_controller.text.isNotEmpty) {
+                      await _databaseService
+                          .editTodo(_controller.text, widget.id!)
+                          .whenComplete(() {
+                        Navigator.of(context).pop();
+                        _controller.clear();
+                      });
+                    }
+                  } else {
+                    await _databaseService
+                        .addTodo(_controller.text)
+                        .whenComplete(() {
+                      _controller.clear();
+                      Navigator.of(context).pop();
+                    });
+                  }
+                },
+                icon: const Icon(CupertinoIcons.add),
+                label: const Text("Add Note"),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,4 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:contineu/screens/auth/login_screen.dart';
-import 'package:contineu/services/auth_service.dart';
+import 'package:contineu/screens/add_todo_screen.dart';
 import 'package:contineu/services/database_service.dart';
 import 'package:contineu/widgets/custom_listtile.dart';
 import 'package:flutter/cupertino.dart';
@@ -40,63 +38,11 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
         trailing: IconButton(
           onPressed: () {
-            showCupertinoModalPopup(
-                context: context,
-                builder: (BuildContext builder) {
-                  return CupertinoPopupSurface(
-                    child: AnimatedPadding(
-                      duration: const Duration(milliseconds: 300),
-                      padding: EdgeInsets.only(
-                          bottom: MediaQuery.of(context).viewInsets.bottom),
-                      child: Container(
-                        padding: EdgeInsets.all(20),
-                        alignment: Alignment.center,
-                        width: double.infinity,
-                        color: Colors.white,
-                        height: 500,
-                        child: Column(
-                          children: [
-                            SizedBox(
-                              height: 100,
-                              child: CupertinoTextField(
-                                controller: _addTodoController,
-                                padding: EdgeInsets.all(10),
-                                decoration: BoxDecoration(
-                                    color: CupertinoColors.systemGrey5,
-                                    borderRadius: BorderRadius.circular(10)),
-                              ),
-                            ),
-                            const SizedBox(
-                              height: 30,
-                            ),
-                            FilledButton(
-                              onPressed: () async {
-                                await _databaseService
-                                    .addTodo(_addTodoController.text)
-                                    .whenComplete(() {
-                                  _addTodoController.clear();
-                                  Navigator.of(context).pop();
-                                });
-                              },
-                              child: Text("Add Task"),
-                              style: ButtonStyle(
-                                backgroundColor: WidgetStatePropertyAll(
-                                    CupertinoColors.darkBackgroundGray),
-                                shape: WidgetStatePropertyAll(
-                                  RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.all(
-                                      Radius.circular(8),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
-                  );
-                });
+            Navigator.of(context).push(
+              CupertinoPageRoute(
+                builder: (context) => AddTodoScreen(),
+              ),
+            );
           },
           icon: Icon(
             CupertinoIcons.add_circled,
@@ -128,79 +74,89 @@ class _HomeScreenState extends State<HomeScreen> {
                         todo['id'],
                       ),
                       onEdit: () {
-                        setState(() {
-                          _editTodoController.text = todo['title'];
-                        });
-                        showCupertinoModalPopup(
-                            context: context,
-                            builder: (BuildContext builder) {
-                              return CupertinoPopupSurface(
-                                child: AnimatedPadding(
-                                  duration: const Duration(milliseconds: 300),
-                                  padding: EdgeInsets.only(
-                                      bottom: MediaQuery.of(context)
-                                          .viewInsets
-                                          .bottom),
-                                  child: Container(
-                                    padding: EdgeInsets.all(20),
-                                    alignment: Alignment.center,
-                                    width: double.infinity,
-                                    color: Colors.white,
-                                    height: 500,
-                                    child: Column(
-                                      children: [
-                                        SizedBox(
-                                          height: 100,
-                                          child: CupertinoTextField(
-                                            maxLength: 30,
-                                            controller: _editTodoController,
-                                            padding: EdgeInsets.all(10),
-                                            decoration: BoxDecoration(
-                                                color:
-                                                    CupertinoColors.systemGrey5,
-                                                borderRadius:
-                                                    BorderRadius.circular(10)),
-                                          ),
-                                        ),
-                                        const SizedBox(
-                                          height: 30,
-                                        ),
-                                        FilledButton(
-                                          onPressed: () async {
-                                            if (_editTodoController
-                                                .text.isNotEmpty) {
-                                              await _databaseService
-                                                  .editTodo(
-                                                      _editTodoController.text,
-                                                      todo['id'])
-                                                  .whenComplete(() {
-                                                Navigator.of(context).pop();
-                                                _editTodoController.clear();
-                                              });
-                                            }
-                                          },
-                                          child: Text("Edit Task"),
-                                          style: ButtonStyle(
-                                            backgroundColor:
-                                                WidgetStatePropertyAll(
-                                                    CupertinoColors
-                                                        .darkBackgroundGray),
-                                            shape: WidgetStatePropertyAll(
-                                              RoundedRectangleBorder(
-                                                borderRadius: BorderRadius.all(
-                                                  Radius.circular(8),
-                                                ),
-                                              ),
-                                            ),
-                                          ),
-                                        )
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                              );
-                            });
+                        Navigator.of(context).push(
+                          CupertinoPageRoute(
+                            builder: (context) => AddTodoScreen(
+                              todoTitle: todo['title'],
+                              id: todo['id'],
+                            ),
+                          ),
+                        );
                       },
+                      // onEdit: () {
+                      //   setState(() {
+                      //     _editTodoController.text = todo['title'];
+                      //   });
+                      //   showCupertinoModalPopup(
+                      //       context: context,
+                      //       builder: (BuildContext builder) {
+                      //         return CupertinoPopupSurface(
+                      //           child: AnimatedPadding(
+                      //             duration: const Duration(milliseconds: 300),
+                      //             padding: EdgeInsets.only(
+                      //                 bottom: MediaQuery.of(context)
+                      //                     .viewInsets
+                      //                     .bottom),
+                      //             child: Container(
+                      //               padding: EdgeInsets.all(20),
+                      //               alignment: Alignment.center,
+                      //               width: double.infinity,
+                      //               color: Colors.white,
+                      //               height: 500,
+                      //               child: Column(
+                      //                 children: [
+                      //                   SizedBox(
+                      //                     height: 100,
+                      //                     child: CupertinoTextField(
+                      //                       maxLength: 30,
+                      //                       controller: _editTodoController,
+                      //                       padding: EdgeInsets.all(10),
+                      //                       decoration: BoxDecoration(
+                      //                           color:
+                      //                               CupertinoColors.systemGrey5,
+                      //                           borderRadius:
+                      //                               BorderRadius.circular(10)),
+                      //                     ),
+                      //                   ),
+                      //                   const SizedBox(
+                      //                     height: 30,
+                      //                   ),
+                      //                   FilledButton(
+                      //                     onPressed: () async {
+                      //                       if (_editTodoController
+                      //                           .text.isNotEmpty) {
+                      //                         await _databaseService
+                      //                             .editTodo(
+                      //                                 _editTodoController.text,
+                      //                                 todo['id'])
+                      //                             .whenComplete(() {
+                      //                           Navigator.of(context).pop();
+                      //                           _editTodoController.clear();
+                      //                         });
+                      //                       }
+                      //                     },
+                      //                     child: Text("Edit Task"),
+                      //                     style: ButtonStyle(
+                      //                       backgroundColor:
+                      //                           WidgetStatePropertyAll(
+                      //                               CupertinoColors
+                      //                                   .darkBackgroundGray),
+                      //                       shape: WidgetStatePropertyAll(
+                      //                         RoundedRectangleBorder(
+                      //                           borderRadius: BorderRadius.all(
+                      //                             Radius.circular(8),
+                      //                           ),
+                      //                         ),
+                      //                       ),
+                      //                     ),
+                      //                   )
+                      //                 ],
+                      //               ),
+                      //             ),
+                      //           ),
+                      //         );
+                      //       });
+                      // },
                     );
                   });
             },
@@ -210,4 +166,3 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 }
-
